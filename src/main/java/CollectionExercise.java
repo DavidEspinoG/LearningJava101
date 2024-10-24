69ab14536d76dcfa76df096e0e90919768ed2d16
David Espino <davidespinog@gmail.com>
2024-10-24 09:28:05 -0600

    anyMatch, allMatch, noneMatch

--- src/main/java/CollectionExercise.java
+++ src/main/java/CollectionExercise.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
 
 class VideoGame {
     private String name;
@@ -44,6 +47,23 @@ class VideoGame {
 public class CollectionExercise {
     public static void main(String[] args) {
         VideoGame halo = new VideoGame("Halo", 1, "Shooter");
-        System.out.println(halo);
+        VideoGame zelda = new VideoGame("Zelda", 1, "Adventure");
+        VideoGame mario = new VideoGame("Mario 64", 1, "Adventure");
+        VideoGame kirby = new VideoGame("Kirby", 1, "Adventure");
+
+        List<VideoGame> videoGames = List.of(halo, zelda, mario, kirby);
+        Predicate<VideoGame> isMultiplayer = videogame -> videogame.getPlayers() > 1;
+        Predicate<VideoGame> areAllOnePlayer = videogame -> videogame.getPlayers() == 1;
+        Predicate<VideoGame> isThereGTA = videogame -> Objects.equals(videogame.getName(), "GTA");
+        boolean anyMultiplayer = videoGames.stream().anyMatch(isMultiplayer);
+        boolean allOnePlayer = videoGames.stream().allMatch(areAllOnePlayer);
+        boolean GTA = videoGames.stream().noneMatch(isThereGTA);
+        System.out.println(anyMultiplayer);
+        System.out.println("All the games are for one player: " + allOnePlayer);
+        System.out.println("We don't have GTA: " + GTA);
+
     }
+
+
+
 }
